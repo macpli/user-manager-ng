357c3725f6d4f9d090195108d3abde276e5ca878
Maciej Pliszek <pliszekmaciej0@gmail.com>
2023-09-12 23:00:49 +0200

    add: confirmation modal for deleting user

--- src/app/components/delete-user/delete-user.component.ts
+++ src/app/components/delete-user/delete-user.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { User } from 'User';
+import { ConfirmDialogComponent } from '../dialogs/confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'app-delete-user',
@@ -8,13 +10,25 @@ import { User } from 'User';
 })
 export class DeleteUserComponent implements OnInit {
   @Input() user!: User;
-
   @Output() onDeleteUser: EventEmitter<User> = new EventEmitter();
   
-  constructor() { }
+  modalInfo = {type : 'delete', isConfirmed: false}
+
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit(): void {
 
+  }
+
+  openModal(user: any) {
+    const modalRef = this.modalService.open(ConfirmDialogComponent);
+    modalRef.componentInstance.modalInfo = this.modalInfo;
+
+    modalRef.result.then((result) => {
+      if(result.isConfirmed) {
+       this.onDelete(user)
+      }
+    })
   }
 
   onDelete(user: any){
